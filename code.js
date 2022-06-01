cc9826101b832936159ede875dfbc448e885c5b2
Evgen112227 <deribasovich@gmail.com>
2022-06-01 20:30:12 +0300

    Added some features

---
+++ code.js
@@ -0,0 +1,4 @@
+let burgerMenuButton = document.querySelector('.header-bottom__burger-menu');
+burgerMenuButton.addEventListener('click', () => {
+    burgerMenuButton.classList.toggle('header-bottom__burger-menu_open');
+})
