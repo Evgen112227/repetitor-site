0e5ed330691f0dd1ae85478bbd3627a62edfd620
Evgen112227 <deribasovich@gmail.com>
2022-06-06 21:55:36 +0300

    .

--- code.js
+++ code.js
@@ -2,3 +2,21 @@ let burgerMenuButton = document.querySelector('.header-bottom__burger-menu');
 burgerMenuButton.addEventListener('click', () => {
     burgerMenuButton.classList.toggle('header-bottom__burger-menu_open');
 })
+
+let map = L.map('map')
+    .setView({
+        lat: 53.943970,
+        lng: 27.696650,
+    }, 15);
+
+let tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+let marker = L.marker({
+    lat: 53.943970,
+    lng: 27.696650
+}).addTo(map);
+
+marker.bindPopup("<b>улица Стариновская, 4</b><br>Минск, Беларусь").openPopup();
