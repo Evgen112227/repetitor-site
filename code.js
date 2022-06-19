f9a4cc9e1f3f3cb886feaf5f1ab8670511eda37c
Evgen112227 <deribasovich@gmail.com>
2022-06-19 16:32:54 +0300

    added slider

--- code.js
+++ code.js
@@ -1,67 +1,36 @@
-let burgerMenuButton = document.querySelector('.header-bottom__burger-menu');
+document.addEventListener('DOMContentLoaded', () => {
+    let burgerMenuButton = document.querySelector('.header-bottom__burger-menu');
 burgerMenuButton.addEventListener('click', () => {
     burgerMenuButton.classList.toggle('header-bottom__burger-menu_open');
 })
 
-let map = L.map('map')
-    .setView({
-        lat: 53.943970,
-        lng: 27.696650,
-    }, 15);
 
-let tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
-
-let marker = L.marker({
-    lat: 53.943970,
-    lng: 27.696650
-}).addTo(map);
-
-marker.bindPopup("<b>улица Стариновская, 4</b><br>Минск, Беларусь").openPopup();
-
-
-//slider
-
-const slides = document.querySelectorAll('.feedback__slide');
-const nextButton = document.querySelector('.button-next');
-const prevButton = document.querySelector('.button-prev');
-let slideIndex = 1;
-slides.forEach(item => item.style.display = 'none');
-showSlides(1);
-
-function timer() {
-    let timerID = setInterval(() => {
-        plusSlides(1);
-    }, 5000);
-    prevButton.addEventListener('click', () => {
-        plusSlides(-1);
-        clearInterval(timerID);
-    });
-
-    nextButton.addEventListener('click', () => {
-        plusSlides(1);
-        clearInterval(timerID);
-    });
-};
-timer();
-
-
-function showSlides(n) {
-    if (n > slides.length) {
-        slideIndex = 1;
+const checkbox = document.querySelector('#checkbox');
+const nav = document.querySelectorAll('.nav__item');
+console.log(nav.firstElementChild)
+const burgerMenu = document.querySelector('.header-bottom__burger-menu');
+document.body.addEventListener('click', (e) => {
+    if(e.target !== burgerMenu.firstElementChild && e.target !== checkbox) {
+        checkbox.checked = false;
+        burgerMenu.classList.remove('header-bottom__burger-menu_open');
     }
+});
 
-    if (n < 1) {
-        slideIndex = slides.length;
-    }
 
-    slides.forEach(item => item.style.display = 'none');
+new Swiper('.image-slider', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    autoplay: {
+        delay: 6000,
+        disableOnInteraction: false,
+        },
+    grabCursor: true,
+});
+});
 
-    slides[slideIndex - 1].style.display = 'block';
-}
 
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
+
+
+
